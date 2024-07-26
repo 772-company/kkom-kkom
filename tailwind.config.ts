71d5172248d397eca7f1f5dc0afb60a291d70f67
leedongkyu <leedong0225@icloud.com>
2024-07-26 16:14:27 +0900

    chore: 이미지파일 삭제 및 테일윈드 폰트설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,8 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+
+      fontFamily: { Pretendard: ["Pretendard", "ui-sans-serif", "system-ui"] },
     },
   },
   plugins: [],

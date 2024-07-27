29922c7f8f581166a86410320dee7fc0dfe33393
codefug <86544979+codefug@users.noreply.github.com>
2024-07-28 04:38:15 +0900

    docs: modal store jsDoc 추가

--- providers/modal-store-provider.tsx
+++ providers/modal-store-provider.tsx
@@ -31,6 +31,21 @@ export const ModalStoreProvider = ({ children }: ModalStoreProviderProps) => {
   );
 };
 
+/**
+ * 모달을 열고 닫는 스토어입니다.
+ * context provider 내에서 사용해야 합니다.
+ * 스토어 전체를 사용하거나 일부만 사용할 수 있습니다.
+ * 스토어 전체를 사용할 경우 하나만 바뀌어도 전체를 사용했던 컴포넌트는 모두 리렌더링됩니다.
+ * 되도록 일부만 사용하는 것이 좋습니다.
+ * @author 이승현
+ * @param selector
+ * @returns store 데이터
+ * @example
+ * // 스토어 전체 사용
+ * const { isOpen, openModal, closeModal } = useModalStore((store) => store);
+ * // 스토어 일부 사용
+ * const { isOpen } = useModalStore((store) => ({ isOpen: store.isOpen }));
+ */
 export const useModalStore = <T,>(selector: (store: ModalStore) => T): T => {
   const modalStoreContext = useContext(ModalStoreContext);
 

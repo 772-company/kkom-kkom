a6a8500a08be9f6d3b665206d9215fbac84000a6
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-07-27 10:18:32 +0900

    chore: tailwind css 커스텀 컬러 설정 (#18)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,58 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        brand: {
+          primary: "#10B981",
+          secondary: "#34D399",
+          tertiary: "#A3E635",
+        },
+        point: {
+          purple: "#A855F7",
+          blue: "#3B82F6",
+          cyan: "#06B6D4",
+          pink: "#EC4899",
+          rose: "#F43F5E",
+          orange: "#F97316",
+          yellow: "#EAB308",
+        },
+        background: {
+          primary: "#0F172A",
+          secondary: "#1E293B",
+          tertiary: "#334155",
+          inverse: "#FFFFFF",
+        },
+        interaction: {
+          inactive: "#94A3B8",
+          hover: "#059669",
+          pressed: "#047857",
+          focus: "#10B981",
+        },
+        border: {
+          primary: "rgba(248, 250, 252, 0.5)",
+        },
+        text: {
+          primary: "#F8FAFC",
+          secondary: "#CBD5E1",
+          tertiary: "#E2E8F0",
+          default: "#64748B",
+          inverse: "#FFFFFF",
+          disabled: "#94A3B8",
+        },
+        status: {
+          danger: "#DC2626",
+        },
+        icon: {
+          primary: "#64748B",
+          inverse: "#F8FAFC",
+          brand: "#10B981",
+        },
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        gradient: "linear-gradient(to right, #10B981, #A3E635)",
       },
     },
   },

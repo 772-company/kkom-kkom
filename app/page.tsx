7b43c529f3bb1d4a2bf2599ca8800707a3407f17
hyunow <stophyun_13@naver.com>
2024-07-27 17:09:49 +0900

    refactor: popover content 버튼 너비, 높이 수정

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Popover from "@/components/popover";
 
 export default function Home() {
-  const content = ["저 귀엽죠", "브이", "ㅋㅋ"];
+  const content = ["칠칠이 파이팅", "칠칠 투 꼼꼼", "렛츠고"];
   return (
     <div className="w-full py-5">
       <div className="flex flex-col items-center gap-1">
@@ -45,12 +45,8 @@ export default function Home() {
         <p className="text-xl text-icon-brand">color</p>
       </div>
       <div className="flex h-[200px] flex-col items-center">
-        <h1>popover test</h1>
         <Popover
-          triggerImageAlt="hamster"
-          triggerText="나는야햄스터"
-          triggerWidth={30}
-          triggerHeight={30}
+          triggerText="팝오버 테스트"
           content={content}
           triggerClassName="bg-pink-200 w-[150px] h-[50px]"
           contentClassName="left-[-76px] h-[100px] w-[200px] bg-yellow-200"
--- components/popover.tsx
+++ components/popover.tsx
@@ -11,10 +11,7 @@ import Image, { StaticImageData } from "next/image";
 import * as React from "react";
 
 const PopOver = PopoverPrimitive.Root;
-
 const PopoverTrigger = PopoverPrimitive.Trigger;
-
-const PopoverAnchor = PopoverPrimitive.Anchor;
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
@@ -26,7 +23,7 @@ const PopoverContent = React.forwardRef<
       align={align}
       sideOffset={sideOffset}
       className={classNames(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border p-4 shadow-md outline-none",
+        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 rounded-md border shadow-md outline-none",
         className,
       )}
       {...props}
@@ -103,7 +100,9 @@ const Popover = ({
         className={`${contentClassName} absolute flex flex-col items-center justify-center border-0 shadow-none`}
       >
         {content.map((item, index) => (
-          <button key={index}>{item}</button>
+          <button className="h-full w-full" key={index}>
+            {item}
+          </button>
         ))}
       </PopoverContent>
     </PopOver>

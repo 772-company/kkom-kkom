58ffd3832661813d8226c99d9aa4859c6f1b9284
leedongkyu <leedong0225@icloud.com>
2024-07-28 22:10:38 +0900

    fix: fowardRef 삭제

--- components/profile-input/profile-input.tsx
+++ components/profile-input/profile-input.tsx
@@ -1,7 +1,7 @@
 import TeamProfile from "@/app/public/icons/img.svg";
 import MyProfile from "@/app/public/icons/my-profile.svg";
 import Image from "next/image";
-import { ChangeEvent, forwardRef, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import React from "react";
 
 interface profileInputProps {
@@ -11,52 +11,51 @@ interface profileInputProps {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const ProfileInput = forwardRef<HTMLInputElement, profileInputProps>(
-  ({ previewImage, image, type, onChange }, ref) => {
-    const [isImgError, setIsImgError] = useState<boolean>(false);
-    return (
-      <>
-        <label htmlFor="profileInput" className="cursor-pointer">
-          {type === "teamProfile" &&
-            (isImgError ? (
-              <TeamProfile width={64} height={64} />
-            ) : (
-              <Image
-                width={64}
-                height={64}
-                src={image}
-                onError={() => setIsImgError(true)}
-                alt="팀이미지"
-              />
-            ))}
+const ProfileInput = ({
+  previewImage,
+  image,
+  type,
+  onChange,
+}: profileInputProps) => {
+  const [isImgError, setIsImgError] = useState<boolean>(false);
+  return (
+    <>
+      <label htmlFor="profileInput" className="cursor-pointer">
+        {type === "teamProfile" &&
+          (isImgError && !previewImage ? (
+            <TeamProfile width={64} height={64} />
+          ) : (
+            <Image
+              width={64}
+              height={64}
+              src={image}
+              onError={() => setIsImgError(true)}
+              alt="팀이미지"
+            />
+          ))}
 
-          {type === "myProfile" &&
-            (isImgError ? (
-              <MyProfile width={64} height={64} />
-            ) : (
-              <Image
-                width={64}
-                height={64}
-                src={previewImage ? previewImage : image}
-                onError={() => setIsImgError(true)}
-                alt="나의이미지"
-              />
-            ))}
-        </label>
+        {type === "myProfile" &&
+          (isImgError && !previewImage ? (
+            <MyProfile width={64} height={64} />
+          ) : (
+            <Image
+              width={64}
+              height={64}
+              src={previewImage ? previewImage : image}
+              onError={() => setIsImgError(true)}
+              alt="나의이미지"
+            />
+          ))}
+      </label>
 
-        <input
-          ref={ref}
-          id="profileInput"
-          type="file"
-          accept=".jpg, jpeg, .png"
-          className="hidden"
-          onChange={onChange}
-        />
-      </>
-    );
-  },
-);
-
-ProfileInput.displayName = "ProfileInput";
-
+      <input
+        id="profileInput"
+        type="file"
+        accept=".jpg, jpeg, .png"
+        className="hidden"
+        onChange={onChange}
+      />
+    </>
+  );
+};
 export default ProfileInput;

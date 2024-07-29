d9dd0d3135814873354fff9065ea87bf814586bf
seo02wow <rlatjdud9188@naver.com>
2024-07-30 01:09:44 +0900

    design: 로그인 페이지 UI 구현

--- schemas/auth.ts
+++ schemas/auth.ts
@@ -3,14 +3,14 @@ import * as yup from "yup";
 export const loginSchema = yup.object().shape({
   email: yup
     .string()
-    .email("email 형식을 입력해주세요")
+    .email("이메일 형식으로 작성해 주세요")
     .required("이메일을 입력해 주세요"),
   password: yup
     .string()
-    .min(8, "비밀번호는 최소 8자리 이상이어야 합니다")
+    .min(8, "비밀번호는 최소 8자 이상입니다.")
     .matches(
       /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/,
-      "비밀번호는 숫자, 영문자, 특수문자(!@#$%^&*)를 포함해야 합니다",
+      "비밀번호는 숫자, 영문, 특수문자로만 가능합니다.",
     )
     .required("비밀번호를 입력해 주세요"),
 });

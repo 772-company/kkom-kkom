3c9e8adaf66a361ddef301f6faf30cfcdc815c17
ldkstellar <116941126+ldkstellar@users.noreply.github.com>
2024-07-29 12:57:11 +0900

    feat: 공통컴퍼넌트 프로필 인풋 팀프로필 인풋 구현 (#34)
    * feat: 공통컴퍼넌트 프로필인풋 팀프로필 인풋 구현
    
    * feat: fowardRef를 사용한 인풋속성 접근 및 테스트코드 삭제
    
    * fix  Component definition is missing display name Error
    
    * fix: fowardRef 삭제
    
    * feat: 테스트 코드 삽입
    
    * fix: 예제코드에서 preview 상태 일때  취소 버튼 누르면 url 저장되서 에러나는 상황고침
    
    * fix : 내부이미지 폭 너비 고정

---
+++ app/test2/page.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import ProfileInput from "@/components/profile-input/profile-input";
+import { useEffect, useState } from "react";
+
+export default function DropdownTestPage() {
+  const [previewImage, setPreviewImage] = useState<string>("");
+  const [image, setImage] = useState<File | null>(null); // server에 업로드할 파일
+  useEffect(() => {
+    return () => {
+      URL.revokeObjectURL(previewImage);
+    };
+  }, []);
+
+  return (
+    <>
+      <ProfileInput
+        type="myProfile"
+        image=""
+        previewImage={previewImage}
+        onChange={(e) => {
+          if (e.target.files) {
+            const file = e.target.files[0];
+            if (file) {
+              const preview = URL.createObjectURL(e.target.files[0]);
+              setImage(e.target.files[0]);
+              setPreviewImage(preview);
+            }
+          }
+        }}
+      />
+    </>
+  );
+}
---
+++ components/profile-input/profile-input.tsx
@@ -0,0 +1,68 @@
+import TeamProfile from "@/app/public/icons/img.svg";
+import MyProfile from "@/app/public/icons/my-profile.svg";
+import Image from "next/image";
+import { ChangeEvent, useState } from "react";
+import React from "react";
+
+/**
+ * @author : 이동규
+ * @typedef {Object} ProfileInputProps
+ * @property {string} image - 서버에서 받아온 유저 이미지 URL
+ * @property {string} preview - 미리보기 이미지 URL
+ **/
+
+interface profileInputProps {
+  previewImage: string;
+  image: string;
+  type: "teamProfile" | "myProfile";
+  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+const ProfileInput = ({
+  previewImage,
+  image,
+  type,
+  onChange,
+}: profileInputProps) => {
+  const [isImgError, setIsImgError] = useState<boolean>(false);
+  return (
+    <div className="h-16 w-16">
+      <label htmlFor="profileInput" className="cursor-pointer">
+        {type === "teamProfile" &&
+          (isImgError && !previewImage ? (
+            <TeamProfile width={64} height={64} />
+          ) : (
+            <Image
+              width={64}
+              height={64}
+              src={image}
+              onError={() => setIsImgError(true)}
+              alt="팀이미지"
+            />
+          ))}
+
+        {type === "myProfile" &&
+          (isImgError && !previewImage ? (
+            <MyProfile width={64} height={64} />
+          ) : (
+            <Image
+              width={64}
+              height={64}
+              src={previewImage ? previewImage : image}
+              onError={() => setIsImgError(true)}
+              alt="나의이미지"
+            />
+          ))}
+      </label>
+
+      <input
+        id="profileInput"
+        type="file"
+        accept=".jpg, jpeg, .png"
+        className="hidden"
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+export default ProfileInput;

c3de72da8a457d60f721973dc3d0f663ddf8a204
codefug <86544979+codefug@users.noreply.github.com>
2024-07-29 23:21:19 +0900

    refactor: cva를 활용한 link, button component 구현

---
+++ components/button/schema/index.ts
@@ -0,0 +1,64 @@
+import { cva } from "class-variance-authority";
+
+export const BUTTON_STYLE = cva(
+  [
+    "flex",
+    "cursor-pointer",
+    "items-center",
+    "justify-center",
+    "rounded-xl",
+    "font-semibold",
+    "transition-all",
+    "duration-300",
+    "ease-in-out",
+    "hover:scale-[1.02]",
+    "active:scale-[0.98]",
+    "disabled:hover:scale-100",
+    "disabled:active:scale-100",
+    "disabled:cursor-not-allowed",
+    "disabled:opacity-50",
+    "disabled:scale-100",
+  ],
+  {
+    variants: {
+      btnSize: {
+        large: ["h-[48px]", "min-w-[280px]", "text-base", "py-3"],
+        "x-small": ["h-[32px]", "min-w-[74px]", "text-sm", "py-1.5"],
+      },
+      btnStyle: {
+        solid: [
+          "bg-brand-primary",
+          "text-white",
+          " hover:bg-interaction-hover",
+          "active:bg-interaction-pressed",
+          "disabled:bg-interaction-inactive",
+        ],
+        outlined: [
+          "border-brand-primary",
+          "text-brand-primary",
+          "hover:border-interaction-hover hover:text-interaction-hover",
+          "active:border-interaction-pressed",
+          "active:text-interaction-pressed",
+          "disabled:border-interaction-inactive",
+          "disabled:text-interaction-inactive",
+          "border",
+          "border-solid",
+          "bg-white",
+        ],
+        danger: [
+          "bg-status-danger",
+          "text-white",
+          "hover:bg-[#f50808]",
+          "active:bg-[#c22020]",
+        ],
+        outlined_secondary: [
+          "border-text-secondary",
+          "text-text-default",
+          "border",
+          "border-solid",
+        ],
+        gradient: ["bg-gradient", "text-white"],
+      },
+    },
+  },
+);

fc97df92cbcb00c3cfc0a36fc62e335a25665c85
hyunow <stophyun_13@naver.com>
2024-07-30 16:04:32 +0900

    fix: import 경로 수정

--- stories/ButtonFloating.stories.ts
+++ stories/ButtonFloating.stories.ts
@@ -1,4 +1,4 @@
-import ButtonFloating from "@/components/button-floating";
+import ButtonFloating from "@/components/button-floating/button-floating";
 import { Meta, StoryObj } from "@storybook/react";
 
 const meta = {

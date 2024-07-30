ee5b759deb055602f32bbd1165f09f7ffef7f869
ldkstellar <116941126+ldkstellar@users.noreply.github.com>
2024-07-30 16:11:02 +0900

    feat: preview 이미지가 있을 때 x버튼 로직 구현 (#52)
    * feat: preview 이미지가 있을 때 x버튼 로직 구현
    
    * refactor: 리액트훅폼으로 테스트 코드 작성
    
    * refactor: 3항연산자 타입으로 묶음 이벤트 핸들러 구현
    
    * fix: tailwind 오타 삭제

--- components/profile-input/profile-input.tsx
+++ components/profile-input/profile-input.tsx
@@ -1,59 +1,78 @@
 import TeamProfile from "@/public/icons/img.svg";
 import MyProfile from "@/public/icons/my-profile.svg";
+import X from "@/public/icons/x.svg";
 import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import React from "react";
-
-/**
- * @author : 이동규
- * @typedef {Object} ProfileInputProps
- * @property {string} image - 서버에서 받아온 유저 이미지 URL
- * @property {string} preview - 미리보기 이미지 URL
- **/
 
 interface profileInputProps {
   previewImage: string;
   image: string;
   type: "teamProfile" | "myProfile";
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClick: () => void;
 }
+
+/**
+ * @author : 이동규
+ * @typedef {Object} ProfileInputProps
+ * @param {string} image - 서버에서 받아온 유저 이미지 URL
+ * @param {string} preview - 미리보기 이미지 URL
+ **/
 
 const ProfileInput = ({
   previewImage,
   image,
   type,
   onChange,
+  onClick,
 }: profileInputProps) => {
   const [isImgError, setIsImgError] = useState<boolean>(false);
+  const handleError = () => {
+    setIsImgError(true);
+  };
+  const errorImage = isImgError && !previewImage;
+
   return (
     <div className="h-16 w-16">
-      <label htmlFor="profileInput" className="cursor-pointer">
+      <label htmlFor="profileInput" className="relative cursor-pointer">
         {type === "teamProfile" &&
-          (isImgError && !previewImage ? (
+          (errorImage ? (
             <TeamProfile width={64} height={64} />
           ) : (
             <Image
               width={64}
               height={64}
+              className="rounded-full"
               src={image}
-              onError={() => setIsImgError(true)}
+              onError={handleError}
               alt="팀이미지"
             />
           ))}
 
         {type === "myProfile" &&
-          (isImgError && !previewImage ? (
+          (errorImage ? (
             <MyProfile width={64} height={64} />
           ) : (
             <Image
               width={64}
               height={64}
+              className="rounded-full"
               src={previewImage ? previewImage : image}
-              onError={() => setIsImgError(true)}
+              onError={handleError}
               alt="나의이미지"
             />
           ))}
       </label>
+
+      {previewImage && (
+        <button
+          className="h-30 w-30 borde absolute left-12 top-1 rounded-full border-2 border-background-primary bg-background-tertiary"
+          onClick={onClick}
+        >
+          <X width={18} height={18} />
+        </button>
+      )}
 
       <input
         id="profileInput"

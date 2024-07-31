c84524890a5c167e2a5f21fc6577b15dcd7e6fe0
codefug <86544979+codefug@users.noreply.github.com>
2024-07-31 23:55:59 +0900

    fix: 없는 페이지들 더 추가

--- app/(free-board)/board/[boardId]/page.tsx
+++ app/(free-board)/board/[boardId]/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
+// import type { Metadata } from "next";
+
+type Props = {
+  params: { boardId: string };
+};
+
+// TODO - 메타데이터 동적 생성
+// export async function generateMetadata({
+//   params: { boardId },
+// }: Props): Promise<Metadata> {
+//   const product = await fetch(`https://.../${boardId}`).then((res) =>
+//     res.json(),
+//   );
+
+//   return {
+//     title: product.title,
+//   };
+// }
+
+export default function Page({ params: { boardId } }: Props) {
   return <div></div>;
 }

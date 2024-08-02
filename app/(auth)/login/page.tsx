54dfa8ff5bd6f07f7517f23a6b96cbb1a15134d8
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-08-02 14:36:04 +0900

    feat: 회원가입 기능 구현  (#71)
    * feat: 회원가입 페이지 UI 구현 및 스키마 정의
    
    * feat: 회원가입 기능 구현
    
    * refactor: h2에서 h1로 변경 및 불필요한 console.log 제거
    
    * refactor: 닉네임 중복 에러 처리 추가
    
    * remove: 회원가입 에러 토스트 삭제
    
    * refactor: 비밀번호 에러 메세지 변경

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,14 +1,14 @@
 import Link from "next/link";
 
-import EasyLogin from "../_components/easy-login";
+import EasyAuth from "../_components/easy-auth";
 import LoginForm from "./_components/login-form";
 
 export default function LoginPage() {
   return (
     <>
-      <h2 className="mx-auto mb-6 text-center text-2xl font-medium text-text-primary md:mb-[80px] xl:text-[40px]">
+      <h1 className="mx-auto mb-6 text-center text-2xl font-medium text-text-primary md:mb-[80px] xl:text-[40px]">
         로그인
-      </h2>
+      </h1>
       <LoginForm />
       <div className="mt-6">
         <section className="mb-[25px] flex items-center justify-center gap-3">
@@ -16,13 +16,13 @@ export default function LoginPage() {
             아직 계정이 없으신가요 ?
           </p>
           <Link
-            href="/signin"
+            href="/signup"
             className="text-sm font-medium text-brand-primary underline"
           >
             가입하기
           </Link>
         </section>
-        <EasyLogin variant="로그인" />
+        <EasyAuth variant="로그인" />
       </div>
     </>
   );
--- lib/apis/auth/index.ts
+++ lib/apis/auth/index.ts
@@ -1,11 +1,15 @@
 import { LoginInputValue } from "@/app/(auth)/login/_components/login-form";
+import { SignUpInputValue } from "@/app/(auth)/signup/_components/signup-form";
 
-import { PostTeamIdAuthSignupResponse } from "../type";
+import {
+  PostTeamIdAuthSigninResponse,
+  PostTeamIdAuthSignupResponse,
+} from "../type";
 
 // NOTE - 로그인
 export async function login(
   data: LoginInputValue,
-): Promise<PostTeamIdAuthSignupResponse | string> {
+): Promise<PostTeamIdAuthSigninResponse | string> {
   try {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_KKOM_KKOM_URL}/auth/signIn`,
@@ -32,5 +36,38 @@ export async function login(
       return error.message;
     }
     return "무슨 메세지를 리턴해야 함";
+  }
+}
+
+// NOTE - 회원가입
+export async function signUp(
+  data: SignUpInputValue,
+): Promise<PostTeamIdAuthSignupResponse | string> {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_KKOM_KKOM_URL}/auth/signUp`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
+
+    if (!response.ok) {
+      const errorData = await response.json();
+      if (response.status === 400) {
+        return errorData.message;
+      }
+      throw new Error("회원가입을 다시 시도해 주세요");
+    }
+    const result: PostTeamIdAuthSignupResponse = await response.json();
+    return result;
+  } catch (error) {
+    if (error instanceof Error) {
+      return error.message;
+    }
+    return "회원가입을 다시 시도해 주세요";
   }
 }

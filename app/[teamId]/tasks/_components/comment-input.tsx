0e3364f3e6d8de42d4aa3527067d4b70d82bbe2b
leedongkyu <leedong0225@icloud.com>
2024-08-06 16:08:03 +0900

    fix: 댓글인풋으로 비제어 컴퍼넌트로 바꾸면서 버튼는  제어가 되도록 변경

--- app/[teamId]/tasks/_components/comment-input.tsx
+++ app/[teamId]/tasks/_components/comment-input.tsx
@@ -1,22 +1,42 @@
-import React, { ChangeEvent, useState } from "react";
+import React from "react";
+import { useForm } from "react-hook-form";
 
 import TaskButton from "./tasks-button";
 
 const Commentinput = () => {
-  const [value, setValue] = useState<string>("");
-  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
+  const {
+    formState: { isDirty },
+    setValue,
+    register,
+    handleSubmit,
+  } = useForm({
+    mode: "onBlur",
+    defaultValues: { value: "" },
+  });
+  const serveData = (
+    data: { value: string },
+    event?: React.BaseSyntheticEvent,
+  ) => {
+    if (!data.value) {
+      return;
+    }
+    console.log(data.value);
+    setValue("value", "");
   };
+
   return (
-    <div className="relative w-full">
+    <form className="relative w-full" onSubmit={handleSubmit(serveData)}>
       <input
-        value={value}
-        onChange={handleChangeInput}
+        {...register("value")}
         placeholder="댓글을 달아주세요"
-        className="h-[24px] w-full bg-background-secondary text-sm text-text-default text-text-primary placeholder:font-normal"
+        className="bg-b h-[24px] w-full bg-background-secondary text-sm text-text-default text-text-primary placeholder:font-normal"
       />
-      <TaskButton types="submit" disable={value.length === 0 ? true : false} />
-    </div>
+      <TaskButton
+        type="submit"
+        types="submit"
+        disable={isDirty ? false : true}
+      />
+    </form>
   );
 };
 

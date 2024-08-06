77e29467e9bfd57f756ccf281a48e49592d15968
hyunow <stophyun_13@naver.com>
2024-08-06 15:23:04 +0900

    refactor: 안 쓰는 코드 삭제

--- app/[groupId]/page.tsx
+++ app/[groupId]/page.tsx
@@ -3,7 +3,6 @@ import getGroupInfo from "@/lib/apis/group";
 import MemberList from "./_components/member-list";
 import TaskLists from "./_components/task-lists";
 import Team from "./_components/team";
-import { fetchGroupInfo } from "./fetch-group-info";
 
 export default async function TeamPage({
   params,

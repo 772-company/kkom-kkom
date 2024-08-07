8f128082a7b174d537cacbe86a19ff455e766707
hyunow <stophyun_13@naver.com>
2024-08-07 14:47:57 +0900

    refactor: hydration failed error 수정

--- app/[groupId]/_components/task-report.tsx
+++ app/[groupId]/_components/task-report.tsx
@@ -1,7 +1,11 @@
 import { GetTeamIdGroupsIdResponse } from "@/lib/apis/type";
+import dynamic from "next/dynamic";
 
-import TodayTaskProgress from "./today-task-progress";
 import TodayTaskSummary from "./today-task-summary";
+
+const TodayTaskProgress = dynamic(() => import("./today-task-progress"), {
+  ssr: false,
+});
 
 type TaskListType = GetTeamIdGroupsIdResponse["taskLists"][0];
 
@@ -20,14 +24,14 @@ const TaskReport = ({ taskLists }: TaskReportProps) => {
   );
 
   return (
-    <div className="flex flex-col gap-[16px]">
+    <div className="relative flex flex-col gap-[16px]">
       <p className="text-[16px] font-[500] text-text-primary">리포트</p>
-      <div className="flex w-full justify-between rounded-[12px] bg-background-secondary p-[24px]">
+      <div className="flex h-[217px] w-full justify-between rounded-[12px] bg-background-secondary p-[24px]">
         <TodayTaskProgress
           numberOfTasks={NUMBER_OF_TASKS}
           numberOfDone={NUMBER_OF_DONE}
         />
-        <div className="flex flex-col gap-[16px]">
+        <div className="absolute right-[24px] flex flex-col gap-[16px]">
           <TodayTaskSummary type="TODO" number={NUMBER_OF_TASKS} />
           <TodayTaskSummary type="DONE" number={NUMBER_OF_DONE} />
         </div>

aa2df5051827b7d5b9a4c060a296e56466ef689e
codefug <86544979+codefug@users.noreply.github.com>
2024-08-07 12:34:42 +0900

    feat: 자유게시판 layout 구현

--- app/(free-board)/layout.tsx
+++ app/(free-board)/layout.tsx
@@ -1,4 +1,6 @@
 import AddBoard from "./_components/add-board";
+import RankingChart from "./boards/_components/ranking-chart";
+import SearchBar from "./boards/_components/search-bar";
 
 export default function Layout({
   children,
@@ -8,7 +10,18 @@ export default function Layout({
   return (
     <>
       <div className="relative mx-4 md:mx-6 xl:mx-auto xl:max-w-[1200px]">
-        {children}
+        <header className="mt-8">
+          <h1 className="mb-6 text-lg font-bold text-text-primary selection:bg-inherit">
+            자유게시판
+          </h1>
+          <SearchBar />
+        </header>
+        <section className="mt-6 md:mt-8">
+          <section className="border-b border-text-primary border-opacity-10">
+            <RankingChart />
+          </section>
+          {children}
+        </section>
         <AddBoard />
       </div>
     </>

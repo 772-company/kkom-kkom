644ff4defa6ba52302071c1c2976913d56cf3f50
leedongkyu <leedong0225@icloud.com>
2024-08-07 19:50:29 +0900

    feat: task api 요청 함수 작성

---
+++ lib/apis/task/index.ts
@@ -0,0 +1,109 @@
+import { getCookie } from "cookies-next";
+
+import { getTaskListResponse } from "./type";
+
+const accessToken = getCookie("accessToken");
+const URL = process.env.NEXT_PUBLIC_KKOM_KKOM_URL;
+
+const getTaskList = async (
+  groupId: number,
+  id: number,
+  date?: Date,
+): Promise<getTaskListResponse> => {
+  try {
+    const response = await fetch(`${URL}/groups${groupId}/${id}?date=${date}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const patchTaskList = async (groupId: number, id: number, data: any) => {
+  try {
+    const response = await fetch(`${URL}/groups${groupId}/tasl-lists/${id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const deleteTaskList = async (groupId: number, id: number) => {
+  try {
+    const response = await fetch(`${URL}/groups${groupId}/tasl-lists/${id}`, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const postTaskList = async (groupId: number, data: any) => {
+  try {
+    const response = await fetch(`${URL}/groups${groupId}/tasl-lists`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+const pathChangeTaskListIndex = async (
+  groupId: number,
+  id: number,
+  data: any,
+) => {
+  try {
+    const response = await fetch(
+      `${URL}/groups${groupId}/tasl-lists/${id}/order`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      },
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
---
+++ lib/apis/task/type/index.ts
@@ -0,0 +1,23 @@
+export interface getTaskListResponse {
+  displayIndex: number;
+  groupId: number;
+  updatedAt: string;
+  createdAt: string;
+  name: string;
+  id: number;
+  tasks: [
+    {
+      displayIndex: number;
+      commentCount: number;
+      deletedAt: string;
+      recurringId: number;
+      frequency: string;
+      userId: number;
+      date: string;
+      doneAt: string;
+      updatedAt: string;
+      name: string;
+      id: number;
+    },
+  ];
+}

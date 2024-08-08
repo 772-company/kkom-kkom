561bc66f860f9ac584aa83e171b001038ff4da50
codefug <86544979+codefug@users.noreply.github.com>
2024-08-08 17:46:01 +0900

    fix: 팀 이미지 안나오던 버그 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
-    domains: ["example.com"],
+    domains: [
+      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      "example.com",
+    ],
   },
   webpack: (config) => {
     config.module.rules.push({

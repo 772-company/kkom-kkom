7760c2b69e0ae9732d9a4dd57239d2293d3faf1c
hyunow <stophyun_13@naver.com>
2024-08-09 01:23:08 +0900

    feat: 팀명 수정 버튼 활성화 조건 추가

--- app/[groupId]/_components/modal-team-name-edit.tsx
+++ app/[groupId]/_components/modal-team-name-edit.tsx
@@ -2,15 +2,23 @@ import Button from "@/components/button/button";
 import Modal from "@/components/modal/modal";
 import { showToast } from "@/lib/show-toast";
 import XIcon from "@/public/icons/x.svg";
+import { ChangeEvent, useState } from "react";
 
 interface ModalTeamNameEditProps {
   close: () => void;
 }
 
 const ModalTeamNameEdit = ({ close }: ModalTeamNameEditProps) => {
-  const handleClick = () => {
+  const [teamName, setTeamName] = useState("");
+
+  const handleButtonClick = () => {
     showToast("success", <p>팀명이 수정되었습니다</p>);
+    console.log(`${teamName}으로 수정됨`);
     close();
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setTeamName(e.target.value);
   };
 
   return (
@@ -23,9 +31,16 @@ const ModalTeamNameEdit = ({ close }: ModalTeamNameEditProps) => {
           <Modal.Title>팀 이름</Modal.Title>
           <input
             className="rounded-xl border border-border-primary border-opacity-10 bg-background-secondary px-4 py-[13.5px] text-base font-normal text-text-primary placeholder:text-sm placeholder:font-normal placeholder:text-text-default focus:border-2 focus:outline-none"
-            placeholder="팀 이름을 입력해 주세요."
+            placeholder="팀 이름을 입력해 주세요"
+            value={teamName}
+            onChange={handleInputChange}
           />
-          <Button btnSize="large" btnStyle="solid" onClick={handleClick}>
+          <Button
+            btnSize="large"
+            btnStyle="solid"
+            onClick={handleButtonClick}
+            disabled={!teamName.trim()}
+          >
             수정하기
           </Button>
         </div>

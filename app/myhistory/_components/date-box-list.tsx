34023ba4ac00ef03edf54da3aa4ca53c83a31eb8
codefug <86544979+codefug@users.noreply.github.com>
2024-08-08 19:31:23 +0900

    fix: date-box-list myFetch에 맞게 수정

--- app/myhistory/_components/date-box-list.tsx
+++ app/myhistory/_components/date-box-list.tsx
@@ -1,13 +1,15 @@
+import { GetTeamIdUserHistoryResponse } from "@/lib/apis/type";
 import { getUserHistory } from "@/lib/apis/user";
 
 import DateBoxCard from "./date-box-card";
 
 export default async function DateBoxList() {
   const data = await getUserHistory();
-  const history = data[0].tasksDone.reduce((acc, cur) => {
-    acc.set(cur.date, [...(acc.get(cur.date) || []), cur]);
-    return acc;
-  }, new Map());
+  const history: Map<string, GetTeamIdUserHistoryResponse[0]["tasksDone"]> =
+    data[0].tasksDone.reduce((acc, cur) => {
+      acc.set(cur.date, [...(acc.get(cur.date) || []), cur]);
+      return acc;
+    }, new Map());
 
   return (
     <section className="mt-[27px] flex flex-col gap-10 md:mt-6">

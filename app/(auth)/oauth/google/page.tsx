1e04493b1d1f678f1fa956c4b650d236ef0cf119
seo02wow <rlatjdud9188@naver.com>
2024-08-08 21:26:53 +0900

    rename: AuthRedirect 컴포넌트 경로 변경

--- app/(auth)/oauth/google/page.tsx
+++ app/(auth)/oauth/google/page.tsx
@@ -3,8 +3,8 @@ import AuthRedirect from "../_components/auth-redirect";
 export default function Page() {
   return (
     <div>
-      <h2>카카오 로그인 중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h2>
-      <AuthRedirect provider="KAKAO" />
+      <h2>구글 로그인 중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h2>
+      <AuthRedirect provider="GOOGLE" />
     </div>
   );
 }

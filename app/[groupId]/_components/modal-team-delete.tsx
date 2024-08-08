297e24a9c519dbe83781a6860d4c128303345ae4
hyunow <stophyun_13@naver.com>
2024-08-09 01:49:46 +0900

    design: 팀 삭제 모달 title 어미 수정

--- app/[groupId]/_components/modal-team-delete.tsx
+++ app/[groupId]/_components/modal-team-delete.tsx
@@ -17,7 +17,7 @@ const ModalTeamDelete = ({ close, teamName }: ModalTeamDeleteProps) => {
     <Modal close={close} closeOnFocusOut>
       <div className="flex w-full flex-col items-center justify-center gap-[24px] px-[25px] py-[15px]">
         <AlertIcon width={24} height={24} />
-        <Modal.Title>{teamName}을 삭제하시겠습니까?</Modal.Title>
+        <Modal.Title>{teamName}을 삭제하시겠어요?</Modal.Title>
         <Modal.TwoButtonSection
           closeBtnStyle="outlined_secondary"
           confirmBtnStyle="danger"

e34595bfb184cf8e2a73c5a900b31c0a452d66eb
codefug <robot9917@naver.com>
2024-08-08 19:12:15 +0900

    refactor: myFetch 함수 구현 (#104)
    * fix: accessToken 관련 로직 수정 > refreshToken 감지
    
    * fix: 테스트를 위한 next.config 수정
    
    * feat: refresh Rotation을 위한 myFetch 함수 구현
    
    * fix: 기존 코드 일부에 myFetch 로직 적용
    
    * fix: 팀 이미지 안나오던 버그 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["sprint-fe-project.s3.ap-northeast-2.amazonaws.com"],
+    domains: [
+      "sprint-fe-project.s3.ap-northeast-2.amazonaws.com",
+      "example.com",
+    ],
   },
   webpack: (config) => {
     config.module.rules.push({

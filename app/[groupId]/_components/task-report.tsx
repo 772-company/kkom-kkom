3fcb6dd9938680280da4d30ae4735a9823e3ab44
hyunow <112617546+hyunow@users.noreply.github.com>
2024-08-09 14:55:26 +0900

    style: 변수 camelCase로 수정

--- app/[groupId]/_components/task-report.tsx
+++ app/[groupId]/_components/task-report.tsx
@@ -10,11 +10,11 @@ interface TaskReportProps {
 }
 
 const TaskReport = ({ taskLists }: TaskReportProps) => {
-  const NUMBER_OF_DONE = taskLists.reduce((acc, taskList) => {
+  const numberOfDone = taskLists.reduce((acc, taskList) => {
     return acc + taskList.tasks.filter((task) => task.doneAt !== null).length;
   }, 0);
 
-  const NUMBER_OF_TASKS = taskLists.reduce(
+  const numberOfTasks = taskLists.reduce(
     (acc, taskList) => acc + taskList.tasks.length,
     0,
   );
@@ -24,12 +24,12 @@ const TaskReport = ({ taskLists }: TaskReportProps) => {
       <p className="text-[16px] font-[500] text-text-primary">리포트</p>
       <div className="flex h-[217px] w-full justify-between rounded-[12px] bg-background-secondary p-[24px]">
         <TodayTaskProgress
-          numberOfTasks={NUMBER_OF_TASKS}
-          numberOfDone={NUMBER_OF_DONE}
+          numberOfTasks={numberOfTasks}
+          numberOfDone={numberOfDone}
         />
         <div className="absolute right-[24px] flex flex-col gap-[16px]">
-          <TodayTaskSummary type="TODO" number={NUMBER_OF_TASKS} />
-          <TodayTaskSummary type="DONE" number={NUMBER_OF_DONE} />
+          <TodayTaskSummary type="TODO" number={numberOfTasks} />
+          <TodayTaskSummary type="DONE" number={numberOfDone} />
         </div>
       </div>
     </div>

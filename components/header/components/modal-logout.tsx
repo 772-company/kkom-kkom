88b53d754521d19dfde54528d9b6c33efd777f8e
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-08-09 17:38:56 +0900

    feat: 로그아웃 구현  (#109)
    * feat: 로그아웃 기능 구현
    
    * refactor: router push 추가

---
+++ components/header/components/modal-logout.tsx
@@ -0,0 +1,33 @@
+import Modal from "@/components/modal/modal";
+import { deleteCookie } from "cookies-next";
+import { useRouter } from "next/navigation";
+
+interface ModalLogoutProps {
+  close: () => void;
+}
+
+export default function ModalLogout({ close }: ModalLogoutProps) {
+  const router = useRouter();
+  function logout() {
+    router.push("/");
+    deleteCookie("accessToken");
+    deleteCookie("refreshToken");
+    router.refresh();
+    close();
+  }
+
+  return (
+    <Modal close={close} closeOnFocusOut>
+      <div className="px-9">
+        <Modal.Title className="my-6">로그아웃 하시겠어요?</Modal.Title>
+        <Modal.TwoButtonSection
+          closeBtnStyle="outlined_secondary"
+          confirmBtnStyle="danger"
+          buttonDescription="로그아웃"
+          close={close}
+          onClick={logout}
+        />
+      </div>
+    </Modal>
+  );
+}

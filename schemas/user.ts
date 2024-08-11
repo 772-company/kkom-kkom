4357443ad19ce2edb875fea3dce69705244df089
seo02wow <rlatjdud9188@naver.com>
2024-08-12 08:47:08 +0900

    feat: 이미지 스키마 정의

--- schemas/user.ts
+++ schemas/user.ts
@@ -5,5 +5,11 @@ export const updateUserSchemas = yup.object().shape({
     .string()
     .max(30, "닉네임은 최대 30자까지 가능합니다.")
     .required("닉네임을 입력해 주세요."),
-  email: yup.string().email("이메일 형식으로 작성해 주세요"),
+  image: yup
+    .mixed<File>()
+    .test("fileType", "JPG, JPEG, PNG 파일만 가능합니다.", (value) => {
+      if (!value) return true; // NOTE - 파일이 선택되지 않았을 때는 유효성 검사 통과
+      const fileType = value.type;
+      return ["image/jpeg", "image/png", "image/jpg"].includes(fileType);
+    }),
 });

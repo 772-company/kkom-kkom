8c6786c891406e90343def587d053d22e0533f53
codefug <86544979+codefug@users.noreply.github.com>
2024-08-11 22:16:07 +0900

    feat: react-hook-form 로직 연결, 글쓰기 모달 완성

---
+++ app/(free-board)/_components/add-board-modal/schema/index.ts
@@ -0,0 +1,21 @@
+import { ObjectSchema, mixed, object, string } from "yup";
+
+import { FormType } from "..";
+
+function checkIfFilesAreTooBig(file: File): boolean {
+  let valid = true;
+  const size = file.size / 1024 / 1024;
+  if (size > 10) {
+    valid = false;
+  }
+  return valid;
+}
+
+export const articleFormSchema: ObjectSchema<FormType> = object().shape({
+  title: string().required("제목을 꼭 입력해주세요!"),
+  content: string().required("내용을 꼭 입력해주세요!"),
+  image: mixed<File>()
+    .nonNullable("이미지를 꼭 업로드해주세요.")
+    .required("이미지를 꼭 업로드해주세요.")
+    .test("fileSize", "파일 사이즈가 너무 큽니다", checkIfFilesAreTooBig),
+});

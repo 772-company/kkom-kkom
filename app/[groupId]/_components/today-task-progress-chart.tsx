423be05167f42ec2945685fa57d901906e7a93fe
hyunow <stophyun_13@naver.com>
2024-08-11 15:16:56 +0900

    refactor: 리포트 반응형 수정

--- app/[groupId]/_components/today-task-progress-chart.tsx
+++ app/[groupId]/_components/today-task-progress-chart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import { Cell, Pie, PieChart } from "recharts";
+import { useEffect, useState } from "react";
+import { Cell, Pie, PieChart, ResponsiveContainer } from "recharts";
 
 interface TodayTaskProgressChartProps {
   numberOfTasks: number;
@@ -11,6 +12,8 @@ const TodayTaskProgressChart = ({
   numberOfTasks,
   numberOfDone,
 }: TodayTaskProgressChartProps) => {
+  numberOfTasks = 10;
+  numberOfDone = 7;
   const numberOfOngoing = numberOfTasks - numberOfDone;
 
   const todayProgressData = [
@@ -24,35 +27,66 @@ const TodayTaskProgressChart = ({
     },
   ];
 
+  const [outerRadius, setOuterRadius] = useState(85);
+  const [innerRadius, setInnerRadius] = useState(55);
+
+  const updateRadius = () => {
+    const width = window.innerWidth;
+
+    if (width <= 768) {
+      setOuterRadius(70); // mobile
+    } else {
+      setOuterRadius(85); // tablet, pc
+    }
+
+    setInnerRadius(outerRadius - 30); // innerRadius = outerRadius - 30
+  };
+
+  useEffect(() => {
+    updateRadius();
+    window.addEventListener("resize", updateRadius);
+
+    return () => {
+      window.removeEventListener("resize", updateRadius);
+    };
+  }, [outerRadius]);
+
   return (
-    <PieChart width={170} height={170} className="z-5">
-      <defs>
-        <linearGradient id="doneGradient" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="0%" stopColor="rgba(163,230,53,1)" />
-          <stop offset="100%" stopColor="rgba(16,185,129,1)" />
-        </linearGradient>
-      </defs>
-      <Pie
-        data={todayProgressData}
-        dataKey="value"
-        nameKey="name"
-        cx="50%"
-        cy="50%"
-        innerRadius={55}
-        outerRadius={85}
-        cornerRadius={20}
-      >
-        {todayProgressData.map((entry, index) => (
-          <Cell
-            key={`cell-${index}`}
-            fill={
-              entry.name === "numberOfDone" ? "url(#doneGradient)" : "#334155"
-            }
-            strokeWidth={0}
-          />
-        ))}
-      </Pie>
-    </PieChart>
+    <div className="h-[140px] w-[140px] md:h-[170px] md:w-[170px]">
+      <ResponsiveContainer width="100%" height="100%">
+        <PieChart className="z-5">
+          <defs>
+            <linearGradient id="doneGradient" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="0%" stopColor="rgba(163,230,53,1)" />
+              <stop offset="100%" stopColor="rgba(16,185,129,1)" />
+            </linearGradient>
+          </defs>
+          <Pie
+            data={todayProgressData}
+            dataKey="value"
+            nameKey="name"
+            cx="50%"
+            cy="50%"
+            outerRadius={outerRadius}
+            innerRadius={innerRadius}
+            cornerRadius={20}
+          >
+            {todayProgressData.map((entry, index) => (
+              <Cell
+                key={`cell-${index}`}
+                fill={
+                  entry.name === "numberOfDone"
+                    ? "url(#doneGradient)"
+                    : "#334155"
+                }
+                strokeWidth={0}
+              />
+            ))}
+          </Pie>
+        </PieChart>
+      </ResponsiveContainer>
+    </div>
   );
 };
+
 export default TodayTaskProgressChart;

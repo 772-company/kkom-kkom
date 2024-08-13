44ed2543b4910d7879cc16d03cc7633941cb4868
hyunow <stophyun_13@naver.com>
2024-08-13 18:06:35 +0900

    feat: BasicInput 컴포넌트 활용

--- app/(team-setting)/participate-team/_components/participate-team-form.tsx
+++ app/(team-setting)/participate-team/_components/participate-team-form.tsx
@@ -1,29 +1,39 @@
 "use client";
 
 import Button from "@/components/button/button";
+import { BasicInput } from "@/components/input-field/basic-input";
 import { postGroupInvitation } from "@/lib/apis/group";
 import { showToast } from "@/lib/show-toast";
 import { useRouter, useSearchParams } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { useForm } from "react-hook-form";
 
 interface ParticipateTeamFormProps {
   email: string;
+}
+
+interface TeamLinkFormValues {
+  token: string;
 }
 
 const ParticipateTeamForm = ({ email }: ParticipateTeamFormProps) => {
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  const [tokenInput, setTokenInput] = useState(searchParams.get("token") || "");
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setTokenInput(e.target.value);
-  };
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<TeamLinkFormValues>({
+    defaultValues: {
+      token: searchParams.get("token") || "",
+    },
+  });
 
-  const handleButtonClick = async () => {
+  const onSubmit = async (data: TeamLinkFormValues) => {
     try {
       await postGroupInvitation({
         userEmail: email,
-        token: tokenInput,
+        token: data.token,
       });
       showToast("success", "팀 참여에 성공하였습니다.");
       router.push("/");
@@ -37,23 +47,18 @@ const ParticipateTeamForm = ({ email }: ParticipateTeamFormProps) => {
   };
 
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)} className="w-full">
       <div className="flex w-full flex-col gap-[40px]">
         <div className="flex flex-col gap-[12px]">
-          <p className="text-[16px] font-[500]">팀 링크</p>
-          <input
-            className="w-full rounded-xl border border-border-primary border-opacity-10 bg-background-secondary px-4 py-[13.5px] text-base font-normal placeholder:text-sm placeholder:font-normal placeholder:text-text-default focus:border-2 focus:outline-none"
-            value={tokenInput}
-            onChange={handleInputChange}
+          <BasicInput<TeamLinkFormValues>
+            id="token"
+            register={register}
+            label="팀 링크"
+            placeholder="팀 링크를 입력해 주세요."
           />
         </div>
         <div className="flex flex-col gap-[10px]">
-          <Button
-            btnSize="large"
-            btnStyle="solid"
-            disabled={!tokenInput.trim()}
-            onClick={handleButtonClick}
-          >
+          <Button btnSize="large" btnStyle="solid">
             참여하기
           </Button>
           <p className="text-center text-[16px] font-[400]">
@@ -61,7 +66,7 @@ const ParticipateTeamForm = ({ email }: ParticipateTeamFormProps) => {
           </p>
         </div>
       </div>
-    </>
+    </form>
   );
 };
 

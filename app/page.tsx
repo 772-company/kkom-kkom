d6a72e47d01b2f0dfad1c33aa97a335bdeb8673a
seo02wow <rlatjdud9188@naver.com>
2024-08-16 15:31:44 +0900

    feat: 모바일 카드 section 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,34 +1,21 @@
-import { LinkButton } from "@/components/button/button";
-import header from "@/public/images/landing-header-large.png";
-import { hasCookie } from "cookies-next";
-import { cookies } from "next/headers";
+import LandingCard from "@/components/landing/landing-card";
+import LandingFirstCard from "@/components/landing/landing-first-card";
+import LandingHeader from "@/components/landing/landing-header";
+import footer from "@/public/images/landing-footer-large.png";
 import Image from "next/image";
 
 export default function Home() {
-  const isLoggedIn = hasCookie("refreshToken", { cookies });
   return (
     <>
-      <section className="absolute h-[547px] w-full">
-        <Image src={header} fill alt="꼼꼼 랜딩" className="object-cover" />
-        <div className="mt-[55px] flex flex-col items-center justify-center gap-1 md:mt-[100px] xl:mt-[84px]">
-          <h2 className="text-2xl font-semibold text-text-primary md:text-[40px]">
-            함께 만들어가는 투두 리스트 🛠️
-          </h2>
-          <h1 className="z-10 bg-gradient-to-r from-brand-primary to-brand-tertiary bg-clip-text text-[32px] font-semibold text-brand-primary text-transparent md:text-[48px] xl:text-[64px]">
-            kkom-kkom
-          </h1>
-          <LinkButton
-            btnSize="large"
-            btnStyle="gradient"
-            href={isLoggedIn ? "" : "/login"}
-            className="mt-[300px] w-[343px] xl:mt-[360px]"
-          >
-            지금 시작하기
-          </LinkButton>
-        </div>
-      </section>
-      <section></section>
-      <section></section>
+      <LandingHeader />
+      <div className="flex flex-col gap-6 px-4 xl:gap-[80px]">
+        <LandingFirstCard />
+        <LandingCard variant="second" />
+        <LandingCard variant="third" />
+      </div>
+      {/* <footer className="relative h-[547px] w-full">
+        <Image src={footer} fill alt="꼼꼼 랜딩" className="object-cover" />
+      </footer> */}
     </>
   );
 }
---
+++ components/landing/landing-header.tsx
@@ -0,0 +1,33 @@
+import header from "@/public/images/landing-header-large.png";
+import { hasCookie } from "cookies-next";
+import { cookies } from "next/headers";
+import Image from "next/image";
+
+import { LinkButton } from "../button/button";
+
+export default function LandingHeader() {
+  const isLoggedIn = hasCookie("refreshToken", { cookies });
+  return (
+    <section className="relative h-[547px] w-full">
+      <Image src={header} fill alt="꼼꼼 랜딩" className="object-cover" />
+      <div className="mt-[55px] flex flex-col items-center justify-center gap-1 md:mt-[100px] xl:mt-[84px]">
+        <h2 className="text-2xl font-semibold text-text-primary md:text-[40px]">
+          함께 만들어가는 투두 리스트 🛠️
+        </h2>
+        <h1 className="z-10 bg-gradient-to-r from-brand-primary to-brand-tertiary bg-clip-text text-[32px] font-semibold text-brand-primary text-transparent md:text-[48px] xl:text-[64px]">
+          kkom-kkom
+        </h1>
+        {!isLoggedIn && (
+          <LinkButton
+            btnSize="large"
+            btnStyle="gradient"
+            href="/login"
+            className="mt-[340px] w-[343px] xl:mt-[360px]"
+          >
+            지금 시작하기
+          </LinkButton>
+        )}
+      </div>
+    </section>
+  );
+}

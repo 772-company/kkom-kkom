2ab885ad16c948c2b64532522001d67fa7c00775
seo02wow <rlatjdud9188@naver.com>
2024-08-17 15:36:47 +0900

    feat: 로띠 파일 첨부 및 로딩 문구 변경

--- app/(auth)/oauth/google/page.tsx
+++ app/(auth)/oauth/google/page.tsx
@@ -3,7 +3,10 @@ import AuthRedirect from "../_components/auth-redirect";
 export default function Page() {
   return (
     <div>
-      <h2>구글 로그인 중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h2>
+      <h2>
+        구글 계정으로
+        <br /> 로그인하고 있어요.
+      </h2>
       <AuthRedirect provider="GOOGLE" />
     </div>
   );
--- app/(auth)/oauth/kakao/page.tsx
+++ app/(auth)/oauth/kakao/page.tsx
@@ -3,7 +3,10 @@ import AuthRedirect from "../_components/auth-redirect";
 export default function Page() {
   return (
     <div>
-      <h2>카카오 로그인 중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h2>
+      <h2>
+        카카오 계정으로
+        <br /> 로그인하고 있어요.
+      </h2>
       <AuthRedirect provider="KAKAO" />
     </div>
   );

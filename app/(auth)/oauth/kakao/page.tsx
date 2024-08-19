61c22b3a55399392f30e4815be408999307a3c6f
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-08-19 14:22:35 +0900

    feat: 소셜 로그인 로딩 UI 수정  (#180)
    * feat: 로띠 파일 첨부 및 로딩 문구 변경
    
    * chore: 로띠 패키지 다운
    
    * design: 소셜 로그인 로딩 UI 구현
    
    * design: 로띠 스피너에서 지렁이로 변경

--- app/(auth)/oauth/kakao/page.tsx
+++ app/(auth)/oauth/kakao/page.tsx
@@ -1,10 +1,5 @@
 import AuthRedirect from "../_components/auth-redirect";
 
 export default function Page() {
-  return (
-    <div>
-      <h2>카카오 로그인 중!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!</h2>
-      <AuthRedirect provider="KAKAO" />
-    </div>
-  );
+  return <AuthRedirect provider="KAKAO" />;
 }

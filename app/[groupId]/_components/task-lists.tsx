97a13c09cdfad5cd03d77a9c1e6e3170a784367a
hyunow <112617546+hyunow@users.noreply.github.com>
2024-08-19 22:44:36 +0900

    feat: 할 일 목록 드래그앤드롭으로 순서 변경하는 기능 구현 (#181)
    * chore: react-beautiful-dnd 설치
    
    * refactor: 순서 변경 api 오타 수정
    
    * feat: 할 일 목록 클릭 시 리스트 페이지 링크 연결
    
    * feat: 할 일 목록 드래그앤드롭 기능 구현
    
    * refactor: task-list api에 instance 적용
    
    * refacor: router 대신 Link 태그 사용
    
    * refactor: strict mode custom droppable 사용

--- app/[groupId]/_components/task-lists.tsx
+++ app/[groupId]/_components/task-lists.tsx
@@ -3,15 +3,21 @@
 import Popover from "@/components/popover/popover";
 import { useCustomOverlay } from "@/hooks/use-custom-overlay";
 import getGroupInfo from "@/lib/apis/group";
+import { patchChangeTaskListIndex } from "@/lib/apis/task-list";
 import { GetTeamIdGroupsIdResponse } from "@/lib/apis/type";
 import Kebab from "@/public/icons/kebab-small.svg";
 import ProgressDone from "@/public/icons/progress-done.svg";
 import ProgressOngoing from "@/public/icons/progress-ongoing.svg";
 import { useQuery } from "@tanstack/react-query";
+import _debounce from "lodash/debounce";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { DragDropContext, Draggable } from "react-beautiful-dnd";
 
 import ModalTaskListAdd from "./modal/modal-task-list-add";
 import ModalTaskListDelete from "./modal/modal-task-list-delete";
 import ModalTaskListNameEdit from "./modal/modal-task-list-name-edit";
+import { StrictModeDroppable } from "./strict-mode-droppable";
 
 type TaskListType = GetTeamIdGroupsIdResponse["taskLists"][0];
 
@@ -62,10 +68,13 @@ const TaskList = ({ taskList, groupId }: TaskListProps) => {
   const isDone = numberOfDone === taskList.tasks.length ? true : false;
 
   return (
-    <div className="flex h-[40px] items-center justify-between rounded-[12px] bg-background-secondary text-[14px] font-[500] leading-[40px] text-text-primary">
+    <Link
+      href={`/${groupId}/tasks`}
+      className="flex h-[40px] cursor-pointer items-center justify-between rounded-[12px] bg-background-secondary text-[14px] font-[500] leading-[40px] text-text-primary"
+    >
       <div className="flex gap-[12px]">
         <div className={`w-[12px] rounded-l-[12px] ${pointColor}`}></div>
-        <p className="">{taskList.name}</p>
+        <p>{taskList.name}</p>
       </div>
       <div className="flex items-center gap-[10px] pr-[8px]">
         <div className="flex h-[25px] items-center gap-[4px] rounded-[12px] bg-background-primary px-[8px]">
@@ -89,21 +98,63 @@ const TaskList = ({ taskList, groupId }: TaskListProps) => {
           contentClassName="z-10 border-[1px] absolute right-0 bg-background-secondary border-border-primary/10 w-[120px] h-[80px] text-white"
         />
       </div>
-    </div>
+    </Link>
   );
 };
 
 const TaskLists = ({ groupId, isAdmin }: TaskListsProps) => {
+  const ModalTaskListAddOverlay = useCustomOverlay(({ close }) => (
+    <ModalTaskListAdd close={close} groupId={groupId} />
+  ));
+
   const { data } = useQuery({
     queryKey: ["groupInfo"],
     queryFn: () => getGroupInfo({ groupId: groupId }),
   });
 
-  const taskLists = data ? data.taskLists : [];
+  const [taskLists, setTaskLists] = useState<TaskListType[]>(
+    data ? data.taskLists : [],
+  );
 
-  const ModalTaskListAddOverlay = useCustomOverlay(({ close }) => (
-    <ModalTaskListAdd close={close} groupId={groupId} />
-  ));
+  // taskLists 초기화
+  useEffect(() => {
+    if (data) {
+      setTaskLists(data.taskLists);
+    }
+  }, [data]);
+
+  // 드래그가 끝났을 때 호출되는 함수
+  const onDragEnd = (result: any) => {
+    const { source, destination } = result;
+
+    // 드래그 종료 위치가 유효하지 않으면 아무것도 하지 않음
+    if (!destination) {
+      return;
+    }
+
+    // 상태 업데이트를 위해 taskLists 배열 복사
+    const updatedTaskLists = Array.from(taskLists);
+
+    // 드래그한 항목을 원래 위치에서 제거
+    const [movedTaskList] = updatedTaskLists.splice(source.index, 1);
+
+    // 드래그한 항목을 새로운 위치에 삽입
+    updatedTaskLists.splice(destination.index, 0, movedTaskList);
+
+    // 이동한 tasklist의 displayIndex 재설정
+    updatedTaskLists.forEach((taskLists, index) => {
+      taskLists.displayIndex = index;
+    });
+
+    setTaskLists(updatedTaskLists);
+
+    // 모든 taskList의 displayIndex를 서버에 업데이트
+    updatedTaskLists.forEach((taskList, index) => {
+      patchChangeTaskListIndex(groupId, taskList.id, {
+        displayIndex: index,
+      }).catch(console.error);
+    });
+  };
 
   return (
     <div className="flex flex-col gap-[16px]">
@@ -123,15 +174,42 @@ const TaskLists = ({ groupId, isAdmin }: TaskListsProps) => {
           </button>
         )}
       </div>
-      <div className="flex h-[208px] flex-col gap-[10px] overflow-y-scroll scrollbar-custom">
-        {taskLists.length > 0 ? (
-          taskLists.map((taskList) => (
-            <TaskList taskList={taskList} key={taskList.id} groupId={groupId} />
-          ))
-        ) : (
-          <p className="text-text-primary">아직 할 일 목록이 없습니다</p>
-        )}
-      </div>
+      <DragDropContext onDragEnd={onDragEnd}>
+        <StrictModeDroppable droppableId="droppable">
+          {(provided) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              className="flex h-[208px] flex-col gap-[10px] overflow-y-scroll scrollbar-custom"
+            >
+              {taskLists.length > 0 ? (
+                taskLists.map((taskList, index) => (
+                  <Draggable
+                    key={taskList.id.toString()}
+                    draggableId={taskList.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <TaskList taskList={taskList} groupId={groupId} />
+                      </div>
+                    )}
+                  </Draggable>
+                ))
+              ) : (
+                <p className="flex h-full items-center justify-center text-text-default">
+                  아직 할 일 목록이 없습니다.
+                </p>
+              )}
+              {provided.placeholder} {/* 드래그 중 빈 공간 유지 */}
+            </div>
+          )}
+        </StrictModeDroppable>
+      </DragDropContext>
     </div>
   );
 };

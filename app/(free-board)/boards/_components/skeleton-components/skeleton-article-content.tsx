0cd323fbb08abbb255ad770e252435b280300ab1
codefug <86544979+codefug@users.noreply.github.com>
2024-08-21 08:16:29 +0900

    feat: 스켈레톤 컴포넌트 추가

---
+++ app/(free-board)/boards/_components/skeleton-components/skeleton-article-content.tsx
@@ -0,0 +1,5 @@
+export default function SkeletonArticleContent() {
+  return (
+    <section className="h-lvh w-full animate-pulse rounded-lg bg-background-tertiary"></section>
+  );
+}
---
+++ app/(free-board)/boards/_components/skeleton-components/skeleton-comment-form.tsx
@@ -0,0 +1,8 @@
+export default function SkeletonCommentForm() {
+  return (
+    <div className="animate-pulse rounded-lg bg-background-tertiary">
+      <header className="9pt-6 flex h-20 justify-between pb-4 text-lg font-medium md:mt-14 md:text-xl"></header>
+      <section className="mb-6 mt-4 flex h-8 w-8 items-center pb-6"></section>
+    </div>
+  );
+}

014b57b3b8b59e23c73593ba1c76218ba23c1bf1
ssseeo0 <135966211+ssseeo0@users.noreply.github.com>
2024-08-22 15:06:17 +0900

    feat: loading.tsx 스피너 추가, not-found 구현  (#222)
    * feat: not-found 페이지 구현
    
    * design: 소셜 로그인 지렁이에서 스피너로 변경
    
    * feat: 로딩 페이지 스피너 설정

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,9 @@
+import Spinner from "@/public/icons/tube-spinner.svg";
+
 export default function Loading() {
-  return <div className="bg-green-600">Loading...</div>;
+  return (
+    <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">
+      <Spinner width={120} height={120} />
+    </div>
+  );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,27 @@
+import NotFoundAnimation from "@/components/animation/not-found-animation";
+import { LinkButton } from "@/components/button/button";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>404 - Page Not Found</h1>
+    <div className="flex flex-col items-center px-[20px]">
+      <NotFoundAnimation />
+      <div className="relative top-[-70px] flex flex-col items-center gap-4 md:gap-6">
+        <h2 className="text-2xl text-text-primary md:text-4xl">
+          찾을 수 없는 페이지입니다.
+        </h2>
+        <p className="text-center text-sm leading-6 text-text-secondary md:text-lg">
+          요청하신 페이지가 사라졌거나, 잘못된 경로입니다. <br /> 올바른 URL을
+          입력하였는지 확인하세요
+        </p>
+        <LinkButton
+          href="/"
+          className="w-[200px]"
+          btnSize="large"
+          btnStyle="solid"
+        >
+          메인으로 돌아가기
+        </LinkButton>
+      </div>
     </div>
   );
 }

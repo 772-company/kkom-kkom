36aeba7a6da45a130095f5c6f8055530e2bee8c2
codefug <86544979+codefug@users.noreply.github.com>
2024-08-24 19:40:51 +0900

    feat: 랜딩 페이지 이미지 최적화

--- components/landing/landing-header.tsx
+++ components/landing/landing-header.tsx
@@ -11,7 +11,7 @@ export default function LandingHeader() {
   return (
     <section className="relative h-[547px] w-full xl:mx-auto xl:max-w-[1920px]">
       <Image
-        src="/images/landing-header-large.png"
+        src="/images/landing-header-large.webp"
         fill
         alt="할 일 옮기는 캐릭터와 기차"
         className="object-cover"

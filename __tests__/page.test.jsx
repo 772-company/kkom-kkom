409a984a8cc2d17dd1b0655b7b2c88c3ff567e1a
codefug <86544979+codefug@users.noreply.github.com>
2024-08-25 14:05:52 +0900

    chore: jest 세팅

---
+++ __tests__/page.test.jsx
@@ -0,0 +1,18 @@
+// import "@testing-library/jest-dom";
+// import { render, screen } from "@testing-library/react";
+
+// import Page from "../app/page";
+
+// describe("Page", () => {
+//   it("renders a heading", () => {
+//     render(<Page />);
+
+//     const heading = screen.getByRole("heading", { level: 1 });
+
+//     expect(heading).toBeInTheDocument();
+//   });
+// });
+
+test("sum", () => {
+  expect(1 + 1).toBe(2);
+});

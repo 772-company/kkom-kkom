e4a809e4fb84448d23283afb15ef2e20ae99c6bf
codefug <86544979+codefug@users.noreply.github.com>
2024-08-26 13:52:42 +0900

    fix: 정적 이미지 middleware 처리

--- middleware.ts
+++ middleware.ts
@@ -26,5 +26,6 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: "/((?!api|_next/static|_next/image|favicon.ico).*)",
+  matcher:
+    "/((?!api|_next/static|_next/image|images|icons|lotties|fonts|favicon.ico).*)",
 };

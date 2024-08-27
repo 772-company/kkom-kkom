c94ccea8740f73907de505fcf36e30bd29e59d11
leedongkyu <leedong0225@icloud.com>
2024-08-27 18:25:25 +0900

    fix: tailwind 수정

--- app/[groupId]/tasks/_components/todo/todo-list-name.tsx
+++ app/[groupId]/tasks/_components/todo/todo-list-name.tsx
@@ -13,7 +13,7 @@ export default function TodoListName({
   return (
     <button
       {...rest}
-      className={`h-[25px] text-[16px] font-medium ${rest.name === selectedButton?.toString() ? "border- border-b border-solid border-text-tertiary" : "text-text-default"}`}
+      className={`h-[25px] text-[16px] font-medium ${rest.name === selectedButton?.toString() ? "border-b border-solid border-text-tertiary" : "text-text-default"}`}
       onClick={rest.onClick}
       type="button"
     >

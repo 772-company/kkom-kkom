c70b7bb737d321df217eaca90051a27d3c0bb1dc
hyunow <112617546+hyunow@users.noreply.github.com>
2024-08-28 15:04:38 +0900

    refactor: 띄어쓰기, 문구 수정

--- app/[groupId]/tasks/_components/no-items/no-todo.tsx
+++ app/[groupId]/tasks/_components/no-items/no-todo.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function NoTodo() {
   return (
-    <p className="mx-auto mt-[30vh] h-[34px] w-[156px] self-center text-text-default">
-      아직 할 일이 목록이없습니다.
+    <p className="mx-auto mt-[30vh] h-[34px] text-center text-text-default">
+      아직 할 일이 없습니다.
       <br />할 일을 추가해보세요.
     </p>
   );
